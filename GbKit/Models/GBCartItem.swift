c25921e2080a1d2ffa2792c398adebec5ab930ac
Ye Gu <kooyear@gmail.com>
2017-11-06 11:51:29 +1100

    no message

--- GbKit/Models/GBCartItem.swift
+++ GbKit/Models/GBCartItem.swift
@@ -9,10 +9,22 @@
 import Foundation
 import ObjectMapper
 
+enum GBShoppingCartItemStatus : String {
+    //允许结算
+    case allowToCheckOut = "ALLOW_TO_CHECK_OUT"
+    //失效
+    case invalid = "INVALID"
+    //未知
+    case unknown = "UNKNOWN"
+}
+
 class GBCartItem:GBProductItem {
     
     //购物车条目id
     var shoppingCartItemId = ""
+    
+    //购物车条目状态
+    var shoppingCartItemStatus = GBShoppingCartItemStatus.allowToCheckOut
     
     // Mappable
     override func mapping(map: Map) {
