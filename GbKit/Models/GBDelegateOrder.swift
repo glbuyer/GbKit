9c9c8e5544b498598cb96b2e6cb5a2eac9c4cb4a
Ye Gu <kooyear@gmail.com>
2017-11-28 13:24:33 +1100

    no message

--- GbKit/Models/GBDelegateOrder.swift
+++ GbKit/Models/GBDelegateOrder.swift
@@ -10,6 +10,9 @@ import Foundation
 import ObjectMapper
 
 enum GBDelegateOrderStatus : String {
+    
+    //预览代发数据
+    case preView = "PRE_VIEW"
     //等待抢单
     case waitForSnatching = "WAIT_FOR_SNATCHING"
     //等待发货
@@ -27,17 +30,40 @@ class GBDelegateOrder:GBOrder {
     
     //代理订单ID
     var delegateOrderId = ""
-    //代理订单状态
+    
+    //代发订单显示标题
+    var delegateOrderTitle = ""
+    //代发订单图片
+    var delegateOrderImageUrl = ""
+    //商品合计代发价格 = sum ( 商品代发价格 * 代发商品数量 )
+    var localDelegateProductAmount:Double = 0
+    //代发运费 ＝ 代发商品总重量 ＊ 代发买手计算重量（不足一公斤，按一公斤计算）
+    var localDelegateShippingFee:Double = 0
+    //合计价格 = 商品合计代发价格 + 代发运费 + 感谢费
+    var localDelegateOrderAmount:Double = 0
+    //感谢费 <= 订单实收款－代发价格－代发运费
+    var delegateLocalAppreciationFee:Double = 0
+    //代发订单所属地
+    var deleateOrderArea = GBArea()
+    //代发订单状态
     var delegateOrderStatus = GBDelegateOrderStatus.other
+    //代发订单状态显示
+    var delegateOrderStatusDisplay = ""
+    //接单买手对于当前订单可操作内容
+    var originalBuyerOperations = [String]()
+    //该代发订单最大可使用感谢费
+    var delegateLocalAppreciationFeeMaximumLimit:Double = 0
+    //允许抢单的代发买手
+    var allowToDelegateBuyers = [GBBuyer]()
+    
+    
+    
     //代理买手id
     var delegateBuyerId = ""
     //代发买手
     var delegateBuyer:GBDelegateBuyer?
     
-    //代发订单图片
-    var delegateOrderImageUrl = ""
-    //代发订单显示标题
-    var delegateOrderTitle = ""
+
     
 
     //设置代发买手时间
@@ -46,27 +72,6 @@ class GBDelegateOrder:GBOrder {
     //代发商品条目
     var delegateOrderItems = [GBOrderItem]()
     
-    //商品合计代发价格 = sum ( 商品代发价格 * 代发商品数量 )
-    var localDelegateProductAmount:Double = 0
-    //感谢费 <= 订单实收款－代发价格－代发运费
-    var delegateLocalAppreciationFee:Double = 0
-    //代发运费 ＝ 代发商品总重量 ＊ 代发买手计算重量（不足一公斤，按一公斤计算）
-    var localDelegateShippingFee:Double = 0
-    //合计价格 = 商品合计代发价格 + 代发运费 + 感谢费
-    var localDelegateOrderAmount:Double = 0
-    //代发订单所属地
-    var deleateOrderArea = GBArea()
-    
-//    //代发订单，代发买手利润 = 合计价格 - sum (代发商品成本价 * 代发商品数量) - 代发运费
-//    var localDelegateOrderProfit:Double = 0
-
-//
-//    //代发订单全集信息
-//    var delegateOrderFullInfo:GBOrder?
-//
-//    //代发买手 将收款 = 未退款商品代发总价 + 未退款商品运费 + 感谢费
-//    var localDelegateBuyerShouldReceiveAmount = 0.00
-
     
     // Mappable
     override func mapping(map: Map) {
