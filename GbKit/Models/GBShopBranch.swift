de5936303a4690173972e10278402a7c92fb9705
Ye Gu <kooyear@gmail.com>
2017-11-29 13:48:18 +1100

    no message

--- GbKit/Models/GBShopBranch.swift
+++ GbKit/Models/GBShopBranch.swift
@@ -27,8 +27,21 @@ class GBShopBranch:GBShop {
     //子商店描述
     var shopBranchDescription = ""
     
+    //子商店logo url 宽度
+    var shopBranchImageURLWidthPx:Double = 100
+    //子商店logo url 高度
+    var shopBranchImageURLHeightPx:Double = 100
+
+    //子商店呈现范围的地图缩放比例上限，对于 ios app
+    var shopShowingUpperSpanRateForIos:Double = 0
+    //子商店呈现范围的地图缩放比例上限，对于 android app
+    var shopShowingUpperSpanRateForAndroid:Double = 0
     
-    
+    //子商店呈现范围的地图缩放比例下限，对于 ios app
+    var shopShowingLowerSpanRateForIos:Double = 0
+    //子商店呈现范围的地图缩放比例下限，对于 android app
+    var shopShowingLowerSpanRateForAndroid:Double = 0
+
     
     // Mappable
     override func mapping(map: Map) {
